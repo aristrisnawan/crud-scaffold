c3e49b62f2ebba2a74948924b9fe23263f0b4f00
aristrisnawan <aristrisnawan35@gmail.com>
2021-01-21 12:42:39 +0800

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # resources :barangs
-  get 'barangs/index'
+  resources :barangs
   root 'barangs#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
